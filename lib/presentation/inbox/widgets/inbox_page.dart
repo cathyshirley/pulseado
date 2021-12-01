009de075bcd41c200b6344b74673f95d4457aa1b
Catherine Shirley Gerald <catherineshirley2231@gmail.com>
2021-12-01 21:26:01 +0530

    changes in the homepage ui

--- lib/presentation/inbox/widgets/inbox_page.dart
+++ lib/presentation/inbox/widgets/inbox_page.dart
@@ -1,9 +1,38 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:im_animations/im_animations.dart';
 
-class InboxPage extends StatelessWidget {
+class InboxPage extends StatefulWidget {
   final String name;
 
   const InboxPage({required this.name});
+
+  @override
+  State<InboxPage> createState() => _InboxPageState();
+}
+
+class _InboxPageState extends State<InboxPage> {
+  late int bpm;
+  late int _now;
+  late Timer _everySecond;
+
+  @override
+  void initState() {
+    super.initState();
+    bpm = 70;
+    final _random = Random();
+    _everySecond = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      setState(() {
+        if (_random.nextInt(2) == 1) {
+          bpm = bpm + 2;
+        } else {
+          bpm = bpm - 2;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,8 +46,59 @@ class InboxPage extends StatelessWidget {
               style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.w300),
             ),
             Text(
-              name,
+              widget.name,
               style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w300),
+            ),
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(24, 24, 24, 12),
+                      child: Column(
+                        children: [
+                          HeartBeat(
+                            child: Icon(
+                              Icons.favorite,
+                              color: Colors.pink,
+                              size: 72.0,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "${bpm} BPM",
+                            style: TextStyle(
+                                fontSize: 32.0, fontWeight: FontWeight.w300),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Status: ",
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.w300),
+                        ),
+                        Text(
+                          "Normal",
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.green),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 120,
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
